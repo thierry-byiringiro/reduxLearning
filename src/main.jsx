d3922927116e46a9d588c508f9d14646dd8d0f21
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-18 16:20:12 +0200

    added the counter reducer

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import userReducer from './features/user.js'
 import themeReducer from './features/theme.js'
+import  counterSlicer  from "./features/counter.js";
 const store = configureStore({
   reducer: {
     user : userReducer,
-    theme : themeReducer
+    theme : themeReducer,
+    counter : counterSlicer,
   },
 });
 createRoot(document.getElementById("root")).render(
