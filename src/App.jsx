bba79b707abd55b98a8e22c9f8c3532dbd185446
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-18 16:19:59 +0200

    imported the counter app

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import "./App.css";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
 import ChangeTheme from "./components/ChangeTheme";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
-      <div className="flex mx-auto flex-col items-center mt-30 space-y-2">
-        <Profile />
-        <Login />
-        <ChangeTheme/>
+      <div className="flex mx-auto flex-col items-center mt-30">
+        <div className="space-y-2 flex flex-col items-center">
+          <Profile />
+          <Login />
+          <ChangeTheme />
+        </div>
+        <Counter />
       </div>
     </>
   );
