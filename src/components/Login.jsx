bd90c82b14c4325e729997c0cfba997b678dc622
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-18 15:38:04 +0200

    setting the user object that contains the user data using react redux

---
+++ src/components/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login(props) {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "HOPE", age: 21, email: "hope@gmail.com" }));
+        }}
+        className="w-20 h-10 p-1 bg-gray-700 text-white rounded-md"
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => dispatch(logout())}
+        className="w-20 h-10 p-1 bg-red-700 text-black rounded-md"
+      >
+        Logout
+      </button>
+    </>
+  );
+}
+
+export default Login;
